06e8fad06f2f14ac26df179cb6c04d339196d356
skystar96 <skystar_uz@mail.ru>
2022-07-06 03:37:42 +0300

    C1

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,52 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private String series;
+
+    @OneToOne(mappedBy = "car",fetch = FetchType.LAZY)
+    private User user;
+
+    public Car(String model, String series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Car() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+}
